4fb47e7866fa7670332b8f0965ac49e647c43f0d
Macko <prvinsm21@gmail.com>
2023-08-06 01:46:42 +0530

    Files added

---
+++ src/main/java/com/macko/hostit/HostitApplication.java
@@ -0,0 +1,13 @@
+package com.macko.hostit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macko/hostit/HostitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macko.hostit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
